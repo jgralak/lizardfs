58c456807673f4ac1e5d53d4a70579151c5d1282
Kamil Pachulski <kamil.pachulski@skytechnology.pl>
2013-10-29 14:01:23 +0100

    tests: Add GoogleTest framework
    This commit provides capability to make unit-tests using GoogleTest framework.
    Framework is downloaded automatically to source directory (external/gtest*)
    during project configuration.

--- Libraries.cmake
+++ Libraries.cmake
@@ -1,6 +1,17 @@
 find_package(ZLIB REQUIRED)
 find_package(Socket)
 find_package(Threads)
+find_package(Boost COMPONENTS filesystem system regex thread)
 
 find_library(FUSE_LIBRARY fuse)
 message(STATUS "FUSE_LIBRARY: ${FUSE_LIBRARY}")
+
+##############
+# GoogleTest #
+##############
+if(ENABLE_TESTS)
+  if(NOT Boost_FOUND)
+    message(FATAL_ERROR "Boost is required by LizardFS tests")
+  endif()
+  set(BUILD_TESTS ON)
+endif()
